d52539c11b70b4b75d25b89555bb63ebd4a4e992
Przemek G <przemo.grzedzielski@gmail.com>
2014-06-18 15:50:20 +0200

    Added CLI parsing in the simulationProcessor's App

--- simulationDataProcessor/src/main/scala/fr/inria/simulationProcessor/App.scala
+++ simulationDataProcessor/src/main/scala/fr/inria/simulationProcessor/App.scala
@@ -1,15 +1,53 @@
 package fr.inria.simulationProcessor
 
+import org.clapper.argot._
+import ArgotConverters._
+import org.apache.log4j.Logger
+import java.io.IOException
 /**
  * @author ${user.name}
+ * The main object that parses the command line and runs the tool.
  */
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  private val _logger:Logger = Logger.getLogger(this.getClass())
+  
+  private val _parser = new ArgotParser("Simulation data processing tool", preUsage=Some("Version 1.0"), postUsage=Some("Enjoy responsibly! :-D"))
+  
+  private val _database_1_url = _parser.option[String](List("db1","database-1"), "db-name", "Url of the first databse")
+  private val _database_2_url = _parser.option[String](List("db2","database-2"), "db-name", "Url of the second database")
+  private val _fileName = _parser.option[String](List("outputFile"), "file-name", "Name of the output csv file to which the data should be exported")
+  
+  private val _bandwidth = _parser.option[String](List("bandwidth"),"link-bandwidth", "The string describing network link bandwidth, e.g. 2Mbit/s, 400 Kbit/s, etc.")
+  private val _tweetLength = _parser.option[Int](List("tweetLength"), "tweet-length", "The length of a single tweet-message that was the transmission grain in our simulation")
+  private val _emissionFrequency = _parser.option[Int](List("emissionFrequency"), "frequency", "The frequency of spouts' emission into the network link (into the topology, more generally)")
+  
+  private val _cliOptions = List(_database_1_url, _database_2_url, _fileName, _bandwidth, _tweetLength, _emissionFrequency)
+  
+  private def _printSomething() = { 
+	var s = new StringBuilder()
+	
+	_cliOptions.foreach(option => {
+	  s append option.name
+	  s append " : "
+	  s append option.value.mkString
+	  s append ", \n"
+	})
+	
+	println(s.toString)
+  }
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    try {
+      _parser.parse(args)
+      // ... run ...
+      _printSomething
+    }
+    catch {
+      case e: ArgotUsageException => _logger error "Bad Argot CLI parsing usage: "+e.toString
+      case e: IOException => _logger error "I/O exception occurred: "+e.toString
+      case e: Exception => _logger error "An exception occurred: "+e.toString
+    }
   }
 
 }
