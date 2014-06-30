a9e43f64cec9b2930410fc2e698744523b1af89c
Przemek G <przemo.grzedzielski@gmail.com>
2014-06-30 17:43:17 +0200

    Corrected one assertion mistake in InterpolationEmptyValuesSubstitutionTest

--- simulationDataProcessor/src/test/scala/fr/inria/simulationProcessor/util/InterpolationEmptyValuesSubstitutionTest.scala
+++ simulationDataProcessor/src/test/scala/fr/inria/simulationProcessor/util/InterpolationEmptyValuesSubstitutionTest.scala
@@ -120,5 +120,24 @@ class InterpolationEmptyValuesSubstitutionTest {
     assertEquals(new DataRecord("2012-04-05 12:00:05.24", valForSpout = 52, valForBolt = 16), resultList(5)) 
     assertEquals(new DataRecord("2012-04-05 12:00:05.55", valForSpout = 72, valForBolt = 17), resultList(6))
   }
+  
+  @Test
+  def testWithLargerTimeDifferences() = {
+    var l: List[DataRecord] = List(
+      new DataRecord("2012-04-05 12:00:05.05", valForBolt = 5, valForSpout = 5),
+      new DataRecord("2012-04-05 12:00:06.08", valForSpout = 6),
+      new DataRecord("2012-04-05 12:00:07.10", valForBolt = 20, valForSpout = 10),
+      new DataRecord("2012-04-05 12:00:08.12", valForBolt = 12),
+      new DataRecord("2012-04-05 12:00:09.08", valForBolt = 18, valForSpout = 50))
+
+    var resultList: List[DataRecord] = (new EmptyValuesNearestSubstitution(DataRecord.EmptyRecord) with InterpolationEmptyValuesSubstitution).substitute(l)
+
+    assertResult(5) { resultList.size }
+    assertEquals(new DataRecord("2012-04-05 12:00:05.05", valForSpout = 5, valForBolt = 5), resultList(0))
+    assertEquals(new DataRecord("2012-04-05 12:00:06.08", valForSpout = 6, valForBolt = 12), resultList(1))
+    assertEquals(new DataRecord("2012-04-05 12:00:07.10", valForSpout = 10, valForBolt = 20), resultList(2))
+    assertEquals(new DataRecord("2012-04-05 12:00:08.12", valForSpout = 30, valForBolt = 12), resultList(3))
+    assertEquals(new DataRecord("2012-04-05 12:00:09.08", valForSpout = 50, valForBolt = 18), resultList(4))
+  }
 
 }
